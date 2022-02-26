708a5cfd07c5250320dd5cadb83363dced9f90ae
ReynaldKusumahadi <95687841+ReynaldKusumahadi@users.noreply.github.com>
2022-02-26 22:07:01 +1100

    Adding contexts to the HTML render

--- distribuddit_backend/distribuddit/views.py
+++ distribuddit_backend/distribuddit/views.py
@@ -11,29 +11,29 @@ def index(request):
 def post(request):
     page_title = 'Post'
     context = {'page_title': page_title}
-    return render(request, 'distribuddit/post.html')
+    return render(request, 'distribuddit/post.html', context)
 
 def create_subreddit(request):
     page_title = 'Create subreddit'
     context = {'page_title': page_title}
-    return render(request, 'distribuddit/create-subreddit.html')
+    return render(request, 'distribuddit/create-subreddit.html', context)
 
 def submit(request):
     page_title = 'Create a post'
     context = {'page_title': page_title}
-    return render(request, 'distribuddit/submit.html')
+    return render(request, 'distribuddit/submit.html', context)
 
 def subreddit(request):
     page_title = '/r/subreddit'
     context = {'page_title': page_title}
-    return render(request, 'distribuddit/subreddit.html')
+    return render(request, 'distribuddit/subreddit.html', context)
 
 def subscribed(request):
     page_title = 'Subscriptions'
     context = {'page_title': page_title}
-    return render(request, 'distribuddit/subscribed.html')
+    return render(request, 'distribuddit/subscribed.html', context)
 
 def user(request):
     page_title = 'User'
     context = {'page_title': page_title}
-    return render(request, 'distribuddit/user.html')
+    return render(request, 'distribuddit/user.html', context)
