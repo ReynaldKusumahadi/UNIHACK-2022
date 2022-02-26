65f43a882bc13ee9322f4421eb42b9c1604c31b0
ReynaldKusumahadi <95687841+ReynaldKusumahadi@users.noreply.github.com>
2022-02-27 02:02:16 +1100

    Update models.py
    Adding a Model of a Post. A reddit-style post contains a title, url, submitter, text, and the date created.

--- distribuddit_backend/distribuddit/models.py
+++ distribuddit_backend/distribuddit/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+  title = models.CharField(max_length=100)  #Title of the post
+  url = models.URLField()                   #URL of the post
+  submitter = models.ForeignKey(User, on_delete=models.CASCADE) #Who is the poster?
+  text = models.TextField(blank=True, null=True)    #Contents of the post
+  created = models.DateTimeField(auto_now_add=True) #When is the post created?
+  
+  class Meta:
+    ordering = ['-created']
