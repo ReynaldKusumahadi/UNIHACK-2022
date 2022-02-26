6a71857b9845be9e5f27d40b4304abadf1ce8d82
Akbar Fadiansyah <akbarfadi01@gmail.com>
2022-02-26 16:58:08 +1100

    recreate django project

--- distribuddit_Backend/distribuddit_Backend/asgi.py
+++ distribuddit_Backend/distribuddit_Backend/asgi.py
@@ -1,16 +1,16 @@
 """
-ASGI config for distribuddit_Backend project.
+ASGI config for distribuddit_backend project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 """
 
 import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'distribuddit_Backend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'distribuddit_backend.settings')
 
 application = get_asgi_application()
--- distribuddit_Backend/distribuddit_Backend/wsgi.py
+++ distribuddit_Backend/distribuddit_Backend/wsgi.py
@@ -1,16 +1,16 @@
 """
-WSGI config for distribuddit_Backend project.
+WSGI config for distribuddit_backend project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'distribuddit_Backend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'distribuddit_backend.settings')
 
 application = get_wsgi_application()
